869a53faf4d5a1db0757b05825e2e0c5c500e15a
soarpenguin <soarpenguin@gmail.com>
2014-01-09 15:20:17 +0800

    pick a device auto when not pointed.

--- devices.cpp
+++ devices.cpp
@@ -1,7 +1,26 @@
+#include <stdlib.h>
+#include <pcap.h>
+
 #include "devices.h"
 
 device * determine_default_device()
 {
-	return new device("eth0");
-}
+    pcap_if_t *alldevs;
+    pcap_if_t *d;
+    char *s;
+    char errbuf[PCAP_ERRBUF_SIZE+1];
 
+    if (pcap_findalldevs(&alldevs, errbuf) == -1)
+    {
+        fprintf(stderr,"Error in pcap_findalldevs: %s\n",errbuf);
+        exit(1);
+    }
+
+    if ( (s = pcap_lookupdev(errbuf)) == NULL) {
+        fprintf(stderr,"Error in pcap_lookupdev: %s\n",errbuf);
+        return new device("eth0");
+    }
+
+    return new device(s);
+}
+ 
